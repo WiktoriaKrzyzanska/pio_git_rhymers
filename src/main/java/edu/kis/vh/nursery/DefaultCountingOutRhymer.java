d24e568f0d16c41353df9ea046801174ab42a70e
WiktoriaKrzyzanska <97694545+WiktoriaKrzyzanska@users.noreply.github.com>
2023-04-25 18:15:04 +0200

    4.Poprawienie kodu, zmiany w hermetyzacji i usunięcie get/set

--- src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
@@ -2,51 +2,36 @@ package edu.kis.vh.nursery;
 
 public class DefaultCountingOutRhymer {
 
-    public static final int MAX_SIZE = 12;
-    public static final int EMPTY = -1;
-    public static final int DEFAULT_VALUE = -1;
-    private final int[] numbers = new int[getMaxSize()];
+    private static final int MAX_SIZE = 12;
+    private static final int EMPTY = -1;
+    private static final int DEFAULT_VALUE = -1;
+    private final int[] numbers = new int[MAX_SIZE];
 
-    public static int getMaxSize() {
-        return MAX_SIZE;
-    }
+    private int total = EMPTY;
 
-    public static int getEMPTY() {
-        return EMPTY;
-    }
-
-    public static int getDefaultValue() {
-        return DEFAULT_VALUE;
-    }
-
-    public int getTotal() {
-        return total;
-    }
-
-    private int total = getEMPTY();
-
+    
     public void countIn(int in) {
         if (!isFull())
             numbers[++total] = in;
     }
 
     public boolean callCheck() {
-        return total == getEMPTY();
+        return total == EMPTY;
     }
 
     public boolean isFull() {
-        return total == getMaxSize() -1;
+        return total == MAX_SIZE-1;
     }
 
     protected int peekaboo() {
         if (callCheck())
-            return getDefaultValue();
+            return DEFAULT_VALUE;
         return numbers[total];
     }
 
     public int countOut() {
         if (callCheck())
-            return getDefaultValue();
+            return DEFAULT_VALUE;
         return numbers[total--];
     }
 
