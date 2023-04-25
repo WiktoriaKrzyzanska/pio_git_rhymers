80ed3ac12d109e87d3477b43d4851ae13b3d9ddc
brzezin <236700@edu.p.lodz.pl>
2023-04-25 15:13:30 +0200

    Fixed DefaultCountingOutRhymer

--- src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
@@ -2,11 +2,10 @@ package edu.kis.vh.nursery;
 
 public class DefaultCountingOutRhymer {
 
-    public static final int SIZE = 12;
+    public static final int MAX_SIZE = 12;
     public static final int EMPTY = -1;
     public static final int DEFAULT_VALUE = -1;
-    public static final int MAX_CAPACITY = 11;
-    private final int[] numbers = new int[SIZE];
+    private final int[] numbers = new int[MAX_SIZE];
 
     public int total = EMPTY;
 
@@ -20,7 +19,7 @@ public class DefaultCountingOutRhymer {
     }
 
     public boolean isFull() {
-        return total == MAX_CAPACITY;
+        return total == MAX_SIZE-1;
     }
 
     protected int peekaboo() {
